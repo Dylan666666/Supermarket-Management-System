3befa2f4d2d31002e0e053c2a7bcb48156744b41
Dylan <1439620614@qq.com>
2020-09-22 22:20:09 +0800

    frist start

---
+++ src/main/java/com/market/scms/ScmsApplication.java
@@ -0,0 +1,13 @@
+package com.market.scms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/market/scms/ScmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.scms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
