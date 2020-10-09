d83ffcf265e6e4a68a9d315d0ebeee626f409ea2
Dylan <1439620614@qq.com>
2020-10-09 21:41:02 +0800

    add supplier_dao success

--- src/test/java/com/market/scms/dao/SupplierDaoTest.java
+++ src/test/java/com/market/scms/dao/SupplierDaoTest.java
@@ -1,8 +1,13 @@
 package com.market.scms.dao;
 
+import com.market.scms.entity.Supplier;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.Date;
 
 /**
  * @Author: Mr_OO
@@ -11,8 +16,34 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SupplierDaoTest {
+    @Resource
     private SupplierDao supplierDao;
     
+    @Test
+    public void insertTest() {
+        Supplier supplier = new Supplier();
+        supplier.setSupplierName("娃哈哈");
+        supplier.setSupplierAddress("中国");
+        supplier.setSupplierPhone("1");
+        supplier.setSupplierPassword("1");
+        int res = supplierDao.insertSupplier(supplier);
+        System.out.println(res);
+    } 
     
+    @Test
+    public void queryTest() {
+        System.out.println(supplierDao.querySupplierList().get(0).getSupplierName());
+        System.out.println(supplierDao.querySupplierByPhone("1").getSupplierName());
+        System.out.println(supplierDao.supplierLogin("1", "1").getSupplierName());
+    }
+    
+    @Test
+    public void updateTest() {
+        Supplier supplier = supplierDao.querySupplierByPhone("1");
+        supplier.setCreateTime(new Date());
+        supplier.setToken("1");
+        System.out.println(supplierDao.updateSupplier(supplier));
+        System.out.println(supplierDao.findByToken("1").getSupplierName());
+    }
     
 }
